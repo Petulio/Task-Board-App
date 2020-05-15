a1091b5573badfe90f47ff3e8af8e385d60ab3a3
Stefan Leiva <stefan.leiva@gmail.com>
2020-05-15 10:53:05 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,204 +1,220 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
-export default function Home() {
+export function CreateTask({}) {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    var postData = JSON.stringify(data);
+    axios
+      .post("http://localhost:8000/tasks/", postData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((resp) => {
+        console.log(resp);
+      });
+  };
+
   return (
-    <div className="container">
+    <div>
+      <div className="create-card">
+        <div className="create-task-title">Create Task: </div>
+        <div className="test">
+          <form className="form-body" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label> Username: </label>
+              <input name="user" ref={register({ required: true })} />
+
+              <label> Task: </label>
+              <input name="text" ref={register({ required: true })} />
+
+              <label> Task done: </label>
+              <input
+                name="done"
+                type="checkbox"
+                ref={register}
+                value={Boolean}
+              ></input>
+            </div>
+            <input type="submit" />
+          </form>
+          {errors.username && " Username is required! "}
+          {errors.task && " Task text is required! "}
+        </div>
+        <style jsx>{`
+          .create-task-title {
+            color: deepskyblue;
+            font-size: 25px;
+          }
+          .test {
+            margin: 1%;
+            text-align: center;
+            font-size: 20px;
+            border: 1.2px solid grey;
+            border-radius: 25px;
+          }
+          .form-body {
+            margin: 2% 4% 2% 4%;
+          }
+        `}</style>
+      </div>
+    </div>
+  );
+}
+
+export default function Home({ tasks }) {
+  function deleteTask(id) {
+    console.log(id);
+    axios.delete(`http://127.0.0.1:8000/tasks/${id}`, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  }
+
+  return (
+    <div className="app-container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Create Tasks App</title>
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1>
+          Welcome to the <div className="title-color">Tasks App</div>
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
+        <div className="task-board-body">
+          <div className="task-board-title">Current tasks:</div>
+          <div className="task-cards">
+            {tasks.map((task) => (
+              <div className="card" key={task.unique_id}>
+                <div className="card-content">
+                  <div id="container">
+                    <div className="column left up">{task.user}:</div>
+                    <div className="column right up">
+                      <img
+                        src="/images/blue_pen.png"
+                        alt="pencil"
+                        className="icon"
+                      />
+                    </div>
+                    <div className="column right up">
+                      <img
+                        src="/images/blue_trash_can.jpg"
+                        alt="trash can"
+                        className="icon"
+                        onClick={() => deleteTask(task.unique_id)}
+                      />
+                    </div>
+                  </div>
+                  <div id="container">
+                    <div className="column left down">{task.text}</div>
+                    <div className="column right down">{String(task.done)}</div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <CreateTask className="task" />
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-        </a>
+        <a>Powered by Stefan </a>
       </footer>
 
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .title-color {
+          color: deepskyblue;
         }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .app-container {
+          margin: 0%;
+          font-family: "Roboto";
         }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .task-board-body {
+          margin: 0% 14% 5% 14%;
         }
-
-        footer img {
-          margin-left: 0.5rem;
+        .task-board-title {
+          font-size: 25px;
+          color: deepskyblue;
         }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .task-cards {
+          margin: 3% 5% 5% 5%;
         }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
         .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
+          margin: 1% 0% 1% 0%;
+          font-size: 25px;
+          border: 1.2px solid grey;
+          border-radius: 25px;
         }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+        .card:hover {
+          border-color: lightskyblue;
+          transform: scale(1.02);
         }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
+        .card-content {
+          margin: 1% 5% 1% 5%;
         }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
+        #container {
+          display: flex;
         }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        .column.left.up {
+          flex: 1 0 300px;
+          font-size: 22px;
+          font-weight: bold;
+        }
+        .column.right.up {
+          flex: 0 0 50px;
+          font-family: "Roboto-light";
+        }
+        .column.left.down {
+          flex: 1 0 300px;
+          font-size: 20px;
+          font-family: "Roboto-light";
+        }
+        .column.right.down {
+          flex: 0 0 50px;
+        }
+        .icon {
+          width: 25px;
+          height: 25px;
+        }
+        .icon:hover {
+          transform: scale(1.1);
         }
       `}</style>
 
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+        @font-face {
+          font-family: "Roboto";
+          src: url("/fonts/Roboto-Regular.ttf");
         }
-
-        * {
-          box-sizing: border-box;
+        @font-face {
+          font-family: "Roboto-light";
+          src: url("/fonts/Roboto-Light.ttf");
+        }
+        body {
+          margin: 5%;
+        }
+        h1 {
+          text-align: center;
+        }
+        footer {
+          margin-block-start: 3em;
         }
       `}</style>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const resp = axios.get(`http://localhost:8000/tasks/`);
+  const tasks = (await resp).data;
+  return {
+    props: {
+      tasks,
+    },
+  };
 }
